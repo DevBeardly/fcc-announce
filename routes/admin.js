664a86bdbc102a2a1b9e8c0e3f4f66dd17c6c2eb
DevBeardly <tim.graves@timgraves.info>
2018-03-20 15:00:53 -0400

    fix typo on admin routes 'middlewareObj' -> 'middleware'

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ var Announcement = require('../models/announcement');
 var middleware = require('../middleware');
 
 // ADMIN DASHBOARD LANDING
-router.get('/', middlewareObj.isLoggedIn, function (req, res) {
+router.get('/', middleware.isLoggedIn, function (req, res) {
   Announcement.find({}, function (err, allAnnouncements) {
     if (err) {
       console.log(err);
