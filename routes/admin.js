e7172f5c37dcae263de9871faa7fd6475a2cae15
DevBeardly <tim.graves@timgraves.info>
2018-03-20 14:57:57 -0400

    add announcements data to admin dashboard

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,18 @@ var express  = require('express');
 var router   = express.Router();
 var passport = require('passport');
 var User     = require('../models/user');
+var Announcement = require('../models/announcement');
+var middleware = require('../middleware');
 
 // ADMIN DASHBOARD LANDING
-router.get('/', function (req, res) {
-  res.render('admin/index');
+router.get('/', middlewareObj.isLoggedIn, function (req, res) {
+  Announcement.find({}, function (err, allAnnouncements) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('admin/index', { announcements: allAnnouncements });
+    }
+  });
 });
 
 module.exports = router;
