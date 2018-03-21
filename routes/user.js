798cb5a3bf60209327b4f2600514235d00c1e865
DevBeardly <tim.graves@timgraves.info>
2018-03-21 16:44:42 -0400

    implement user update and delete routes

--- routes/user.js
+++ routes/user.js
@@ -9,11 +9,11 @@ router.get('/', middleware.isLoggedIn, function (req, res) {
   res.render('user/index');
 });
 
-// EDIT CURRENT USER INFO ROUTE
+// EDIT USER INFO ROUTE
 router.get('/:id/edit', middleware.isLoggedIn, function (req, res) {
   User.findById(req.params.id, function (err, foundUser) {
     if (err) {
-      req.flash('error', 'Could not find that user.')
+      req.flash('error', 'Could not find that user.');
       res.redirect('back');
     } else {
       res.render('user/edit', { user: foundUser });
@@ -21,9 +21,30 @@ router.get('/:id/edit', middleware.isLoggedIn, function (req, res) {
   });
 });
 
-// UPDATE CURRENT USER INFO ROUTE
-router.put('/', middleware.isLoggedIn, function (req, res) {
-  res.send('You\'ve hit the user UPDATE route.');
+// UPDATE USER INFO ROUTE
+router.put('/:id', middleware.isLoggedIn, function (req, res) {
+  User.findByIdAndUpdate(req.params.id, req.body.user, function (err, updatedUser) {
+    if (err) {
+      req.flash('error', 'Could not find that user.');
+      res.redirect('back');
+    } else {
+      req.flash('success', 'Successfully updated that user!');
+      res.redirect('/admin/users');
+    }
+  });
+});
+
+// DELETE USER INFO ROUTE
+router.delete('/:id', function (req, res) {
+  User.findByIdAndRemove(req.params.id, function (err) {
+    if (err) {
+      req.flash('error', 'Something went wrong with the database.');
+      res.redirect('/admin/users');
+    } else {
+      req.flash('success', 'Successfully deleted that user.');
+      res.redirect('/admin/users');
+    }
+  });
 });
 
 router.get('/reset', middleware.isLoggedIn, function (req, res) {
